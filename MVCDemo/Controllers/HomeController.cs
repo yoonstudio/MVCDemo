a761321d15fb9dc26966ab002af9e639d5765766
YOONSTUDIO <yoonstudio@nate.com>
2022-02-25 14:05:53 +0900

    초기 커밋.

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product SampleProduct = new Product
+        {
+            ProductID = 1,
+            Name = "책상",
+            Description = "사무용",
+            Category = "가구",
+            Priced = 100000
+        };
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Razor~";
+            return View(SampleProduct);
+        }
+
+        public ActionResult ProductInfo()
+        {
+            return View(SampleProduct);
+        }
+
+        public ActionResult SampleAction()
+        {
+            ViewBag.OrderCount = 3;
+            ViewBag.ProductCount = 1;
+            ViewBag.Discount = true;
+            ViewBag.ShipPrice = false;
+
+            return View(SampleProduct);
+        }
+
+        public ActionResult ArraySampleAction()
+        {
+            Product[] array =
+                {
+                new Product {Name = "점퍼", Priced = 60000},
+                new Product {Name = "농구공", Priced = 30000},
+                new Product {Name = "TV", Priced = 1000000}
+                };
+
+            return View(array);
+        }
+
+        public ActionResult AutoProperty()
+        {
+            Product product = new Product();
+
+            product.Name = "pd_01";
+
+            string productName = product.Name;
+
+            return View("Result", (object)string.Format($"상품명: {productName}"));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCDemo/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class Product
+    {
+        private int productID;
+        private string category;
+        private string name;
+        private string description;
+        private int price;
+
+        public int ProductID { get; set; }
+        public string Category { get { return category; } set { category = value; } }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Priced { get { return productID + price; } set { productID = value; } }
+    }
+}
